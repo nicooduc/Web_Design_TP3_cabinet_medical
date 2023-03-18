a0c90de7a1d1d22bc0a411b2dbaed7416b2fd68b
nicooduc <38563138+nicooduc@users.noreply.github.com>
2023-03-18 16:47:53 +0100

    Delete .history directory

--- .history/autoload_20230318153820.php
+++
@@ -1,7 +0,0 @@
-<?php
-function autoload($class_name)
-{
-    require_once __DIR__ . '/classes/' . $class_name . '.php';
-}
-
-?>
--- .history/autoload_20230318153903.php
+++
@@ -1,7 +0,0 @@
-<?php
-function autoload($class_name)
-{
-    require_once __DIR__ . 'Controller/' . $class_name . '.php';
-}
-
-?>
